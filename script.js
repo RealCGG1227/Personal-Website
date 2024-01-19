67bb3f7ab98cd181209fec8d7f9d7a2263bd9263
Cyril Adolar <cy_cy01@Cyrils-MacBook-Air.local>
2024-01-19 23:20:49 +0800

    full webpage

---
+++ script.js
@@ -0,0 +1,7 @@
+function achievements(){
+    alert("Most Generous Gr.4 & Gr. 5\n1st Runner up Mr. STRS");
+}
+
+function skills(){
+    alert("Troubleshooting on Devices\nGaming on PC\nDriving [car only]");
+}
